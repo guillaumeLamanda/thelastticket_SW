bde5ef6db0022413de6cd5f24474d9af9c4cbaad
lamanda <guill29640@gmail.com>
2017-01-28 15:11:55 +0100

    Get form callback

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -7,9 +7,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'page1.html'
 })
 export class Page1 {
-
+  id: number;
+  username: string;
+  password: string;
   constructor(public navCtrl: NavController) {
-    
+    this.id = 1;
+    this.username = '';
+    this.password = '';
+  }
+
+  // Log the user
+  login() {
+    console.log(this.username);
+    console.log('user logged');
   }
 
 }
