e2ee3de9d407ae630b4acc372bd2dafa2e525518
cesar.fraisseix <cesar.fraisseix@gmail.com>
2017-01-29 11:40:18 +0100

    Continued workin on buying page

--- src/pages/page2/page2.ts
+++ src/pages/page2/page2.ts
@@ -11,7 +11,7 @@ export class Page2 {
   selectedItem: any;
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
-  shows: Array<{id: number, Author: any, Title: string, Date: string, Place: string, Image: string, Price: number}>;
+  shows: Array<{id: number, Author: any, Title: string, Date: string, Time: string, Place: string, Image: string, Price: number}>;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
@@ -26,6 +26,7 @@ export class Page2 {
         },
         Title: "Concert Stromae",
         Date: "28 Janvier",
+        Time: "20",
         Place: "Zenith, Paris",
         Image: "img/show5.jpg",
         Price: 79.50
@@ -38,6 +39,7 @@ export class Page2 {
         },
         Title: "Ligue 1 - PSG/Marseille",
         Date: "29 Janvier",
+        Time: "20",
         Place: "Stade de France, Paris",
         Image: "img/show2.jpg",
         Price: 56
@@ -50,6 +52,7 @@ export class Page2 {
         },
         Title: "Concert U2",
         Date: "2 Février",
+        Time: "20",
         Place: "Zenith, Paris",
         Image: "img/show3.jpg",
         Price: 110
@@ -62,6 +65,7 @@ export class Page2 {
         },
         Title: "Carmen",
         Date: "29 janvier",
+        Time: "20",
         Place: "Opéra de Paris",
         Image: "img/show4.jpg",
         Price: 46
@@ -74,6 +78,7 @@ export class Page2 {
         },
         Title: "Concert Taylor Swift",
         Date: "28 janvier",
+        Time: "20",
         Place: "Zenith, Paris",
         Image: "img/show1.jpg",
         Price: 86
@@ -86,6 +91,7 @@ export class Page2 {
         },
         Title: "Gad sur scène",
         Date: "29 janvier",
+        Time: "20",
         Place: "Point Virgule, Paris",
         Image: "img/show6.jpg",
         Price: 22
